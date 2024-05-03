2d1017f3809c7b6496452dc4dc520599d88594a9
arturJan4 <27348543+arturJan4@users.noreply.github.com>
2024-05-03 15:00:04 +0200

    Add basic TextNode class and tests for TextNode

---
+++ StaticSiteGenerator/src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+
+def main():
+    example = TextNode("example test node", "italic", "example.com")
+    print(example)
+
+
+if __name__ == "__main__":
+    main()
---
+++ StaticSiteGenerator/src/textnode.py
@@ -0,0 +1,19 @@
+class TextNode():
+    def __init__(self, text, text_type, url=None) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, value: object) -> bool:
+        if self.text != value.text:
+            return False
+        if self.text_type != value.text_type:
+            return False
+        if self.url != value.url:
+            return False
+        return True
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+    
