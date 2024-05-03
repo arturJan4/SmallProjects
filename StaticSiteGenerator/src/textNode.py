2d398d083dea67efb5bdd11115c39efcc2f17575
arturJan4 <27348543+arturJan4@users.noreply.github.com>
2024-05-03 17:45:29 +0200

    Add text node to HTML node conversion

--- StaticSiteGenerator/src/textNode.py
+++ StaticSiteGenerator/src/textNode.py
@@ -1,4 +1,18 @@
-class TextNode():
+from enum import StrEnum
+
+from src.HTMLNode import LeafNode
+
+
+class TextTypes(StrEnum):
+    TEXT = ("text",)
+    BOLD = ("bold",)
+    ITALIC = ("italic",)
+    CODE = ("code",)
+    LINK = ("link",)
+    IMAGE = "image"
+
+
+class TextNode:
     def __init__(self, text, text_type, url=None) -> None:
         self.text = text
         self.text_type = text_type
@@ -15,3 +29,21 @@ class TextNode():
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node: "TextNode"):
+    if text_node.text_type == TextTypes.TEXT.value:
+        return LeafNode(value=text_node.text)
+    elif text_node.text_type == TextTypes.BOLD.value:
+        return LeafNode(tag="b", value=text_node.text)
+    elif text_node.text_type == TextTypes.ITALIC.value:
+        return LeafNode(tag="i", value=text_node.text)
+    elif text_node.text_type == TextTypes.CODE.value:
+        return LeafNode(tag="code", value=text_node.text)
+    elif text_node.text_type == TextTypes.LINK.value:
+        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+    elif text_node.text_type == TextTypes.IMAGE.value:
+        return LeafNode(
+            tag="img", value="", props={"src": text_node.url, "alt": text_node.text}
+        )
+    raise ValueError(f"Text type ({text_node.text_type}) not recognized")
--- StaticSiteGenerator/tests/tests_textNode.py
+++ StaticSiteGenerator/tests/tests_textNode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from src.textNode import TextNode
+from src.textNode import TextNode, text_node_to_html_node
+from src.HTMLNode import LeafNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -9,16 +10,61 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
 
-
     def test_neq(self):
         node = TextNode("This is a text node", "bold")
         node2 = TextNode("This is a text node", "italic")
         self.assertNotEqual(node, node2)
 
-
     def test_repr(self):
         node = TextNode("a", "b", url="c")
         self.assertEqual(repr(node), "TextNode(a, b, c)")
+
+
+class TestConvertTextToHTML(unittest.TestCase):
+    def test_convert_html_normal(self):
+        node = TextNode("text node", "text")
+        expected = LeafNode(tag=None, value="text node")
+        actual = text_node_to_html_node(node)
+        self.assertEqual(actual, expected)
+
+    def test_convert_html_bold(self):
+        node = TextNode("text node", "bold")
+        expected = LeafNode(tag="b", value="text node")
+        actual = text_node_to_html_node(node)
+        self.assertEqual(actual, expected)
+
+    def test_convert_html_italic(self):
+        node = TextNode("text node", "italic")
+        expected = LeafNode(tag="i", value="text node")
+        actual = text_node_to_html_node(node)
+        self.assertEqual(actual, expected)
+
+    def test_convert_html_code(self):
+        node = TextNode("text node", "code")
+        expected = LeafNode(tag="code", value="text node")
+        actual = text_node_to_html_node(node)
+        self.assertEqual(actual, expected)
+
+    def test_convert_html_link(self):
+        txt = "anchor text"
+        url = "example.org"
+        node = TextNode(text=txt, text_type="link", url=url)
+        expected = LeafNode(tag="a", value=txt, props={"href": url})
+        actual = text_node_to_html_node(node)
+        self.assertEqual(actual, expected)
+
+    def test_convert_html_image(self):
+        alt_text = "picture description"
+        src = "example.jpg"
+        node = TextNode(text=alt_text, text_type="image", url=src)
+        expected = LeafNode(tag="img", value="", props={"src": src, "alt": alt_text})
+        actual = text_node_to_html_node(node)
+        self.assertEqual(actual, expected)
+
+    def test_convert_html_error(self):
+        with self.assertRaises(ValueError):
+            node = TextNode(text="", text_type="pdf")
+            test = text_node_to_html_node(node)
 
 
 if __name__ == "__main__":
