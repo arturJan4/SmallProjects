6df0e7246a809d7afdc1841179d91f57d35455d3
arturJan4 <27348543+arturJan4@users.noreply.github.com>
2024-05-03 15:01:58 +0200

    Add more TextNode tests

--- StaticSiteGenerator/tests/test_textnode.py
+++ StaticSiteGenerator/tests/test_textnode.py
@@ -10,5 +10,16 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node, node2)
 
 
+    def test_neq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "italic")
+        self.assertNotEqual(node, node2)
+
+
+    def test_repr(self):
+        node = TextNode("a", "b", url="c")
+        self.assertEqual(repr(node), "TextNode(a, b, c)")
+
+
 if __name__ == "__main__":
     unittest.main()
