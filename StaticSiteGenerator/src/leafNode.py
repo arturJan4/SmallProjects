bc23a61e1fd0c1bc78c9470f24c2d5121237fbe7
arturJan4 <27348543+arturJan4@users.noreply.github.com>
2024-05-03 15:41:15 +0200

    Add LeafNode

---
+++ StaticSiteGenerator/src/leafNode.py
@@ -0,0 +1,18 @@
+from src.HTMLNode import HTMLNode
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None) -> None:
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Leaf nodes must have a value.")
+
+        if self.tag is None:
+            return self.value
+
+        tag_attributes = self.props_to_html()
+        starting_tag = f"<{self.tag} {tag_attributes}>"
+        closing_tag = f"</{self.tag}>"
+        return starting_tag + self.value + closing_tag
---
+++ StaticSiteGenerator/tests/tests_leafNode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from src.leafNode import LeafNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_eq(self):
+        node = LeafNode("h1", "text", {"class": "cl1"})
+        node2 = LeafNode("h1", "text", {"class": "cl1"})
+        self.assertEqual(node, node2)
+
+    def test_neq(self):
+        node = LeafNode("h1", "text", {"class": "cl1"})
+        node2 = LeafNode("h1", "text", {"class": "cl2"})
+        self.assertNotEqual(node, node2)
+
+    def test_to_html(self):
+        node = LeafNode("p", "text", {"class": "cl1"})
+        self.assertEqual(node.to_html(), '<p class="cl1">text</p>')
+
+        node = LeafNode("a", "Click me!",
+                        {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), 
+                         '<a href="https://www.google.com">Click me!</a>')
+
+if __name__ == "__main__":
+    unittest.main()
