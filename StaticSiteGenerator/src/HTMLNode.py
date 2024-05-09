0a633ff2a5f574ed06c62c33889aac2dcd50c7e3
arturJan4 <27348543+arturJan4@users.noreply.github.com>
2024-05-09 15:41:44 +0200

    Refactor HTMLNode with DRY

--- StaticSiteGenerator/src/HTMLNode.py
+++ StaticSiteGenerator/src/HTMLNode.py
@@ -17,12 +17,15 @@ class HTMLNode:
         return True
 
     def __repr__(self) -> str:
+        # string representation used for debugging
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
     def to_html(self):
+        # convert node to HTML that's possible to render
         raise NotImplementedError("to_html() not implemented")
 
     def props_to_html(self):
+        # properties in html are a sequence of key=value assignments
         if not self.props or len(self.props) == 0:
             return ""
         html = []
@@ -30,8 +33,18 @@ class HTMLNode:
             html.append(f'{key}="{value}"')
         return " ".join(html)
 
+    def __get_starting_and_closing_tags__(self):
+        starting_tag = f"<{self.tag}>"
+        tag_attributes = self.props_to_html()
+        if len(tag_attributes) > 0:
+            starting_tag = f"<{self.tag} {tag_attributes}>"
+        closing_tag = f"</{self.tag}>"
+
+        return starting_tag, closing_tag
+
 
 class LeafNode(HTMLNode):
+    # Leaf Node is a node that has no children under it
     def __init__(self, tag=None, value=None, props=None) -> None:
         super().__init__(tag, value, None, props)
 
@@ -42,16 +55,13 @@ class LeafNode(HTMLNode):
         if self.tag is None:
             return self.value
 
-        starting_tag = f"<{self.tag}>"
-        tag_attributes = self.props_to_html()
-        if len(tag_attributes) > 0:
-            starting_tag = f"<{self.tag} {tag_attributes}>"
+        starting_tag, closing_tag = self.__get_starting_and_closing_tags__()
 
-        closing_tag = f"</{self.tag}>"
         return starting_tag + self.value + closing_tag
 
 
 class ParentNode(HTMLNode):
+    # Parent Node is a node with other parent nodes or leaves as children
     def __init__(self, children, tag=None, props=None) -> None:
         super().__init__(tag, None, children, props)
 
@@ -62,11 +72,7 @@ class ParentNode(HTMLNode):
         if self.tag is None:
             raise ValueError("Can't produce HTML with no given tag in parent Node")
 
-        starting_tag = f"<{self.tag}>"
-        tag_attributes = self.props_to_html()
-        if len(tag_attributes) > 0:
-            starting_tag = f"<{self.tag} {tag_attributes}>"
-        closing_tag = f"</{self.tag}>"
+        starting_tag, closing_tag = self.__get_starting_and_closing_tags__()
 
         # go through children nodes and recursively generate HTML
         inner_html = ""
