df9fbe59182880e6f888af577331546ff737b8c4
arturJan4 <27348543+arturJan4@users.noreply.github.com>
2024-05-10 21:54:21 +0200

    Added markdown to HTML Nodes Tree functionality

--- StaticSiteGenerator/src/markdown.py
+++ StaticSiteGenerator/src/markdown.py
@@ -1,14 +1,23 @@
-from src.HTMLNode import HTMLNode, LeafNode, ParentNode
+from src.HTMLNode import HTMLNode, ParentNode
 from src.markdownBlock import BlockTypes, block_to_block_type, markdown_to_blocks
-from src.markdownText import markdown_text_to_textNodes
+from src.markdownText import TextNode, TextTypes, markdown_text_to_textNodes
 from src.textNode import text_node_to_html_node
 
 
 def markdown_to_html(markdown) -> HTMLNode:
+    """
+    HTML is rendered as a big div with children inside being blocks.
+    Each Block is rendered as a list of children with associated tag.
+    """
     blocks = markdown_to_blocks(markdown)
 
     block_to_HTML_functions = {
         BlockTypes.PARAGRAPH: paragraph_to_html,
+        BlockTypes.QUOTE: quoteblock_to_html,
+        BlockTypes.CODE: codeblock_to_html,
+        BlockTypes.HEADING: heading_to_html,
+        BlockTypes.UNORDERED_LIST: unordered_list_to_html,
+        BlockTypes.ORDERED_LIST: ordered_list_to_html,
     }
 
     div_children = []
@@ -30,3 +39,76 @@ def paragraph_to_html(block: str) -> HTMLNode:
         children.append(text_node_to_html_node(text_node))
 
     return ParentNode(children, "p", None)
+
+
+def quoteblock_to_html(block: str) -> HTMLNode:
+    """Identified by > at start of every line"""
+    children = []
+
+    # remove starting '>'
+    block = "\n".join(map(lambda x: x[1:], block.split("\n")))
+
+    text_nodes = markdown_text_to_textNodes(block)
+
+    for text_node in text_nodes:
+        children.append(text_node_to_html_node(text_node))
+
+    return ParentNode(children, "blockquote", None)
+
+
+def codeblock_to_html(block: str) -> HTMLNode:
+    """Identified by ``` at start and end. Treat inside as a text"""
+    code_node = TextNode(block[3:-3], TextTypes.TEXT)
+    html_node = text_node_to_html_node(code_node)
+
+    return ParentNode([html_node], "code", None)
+
+
+def heading_to_html(block: str) -> HTMLNode:
+    hashes, block = block.split(" ", maxsplit=1)
+    strength = hashes.count("#")
+    text_nodes = markdown_text_to_textNodes(block)
+
+    children = []
+    for text_node in text_nodes:
+        children.append(text_node_to_html_node(text_node))
+
+    return ParentNode(children, f"h{strength}", None)
+
+
+def unordered_list_to_html(block: str) -> HTMLNode:
+    """Each item should be enclosed in li tag"""
+    children = []
+
+    lines = list(map(lambda x: x[2:], block.splitlines()))
+
+    for line in lines:
+        text_nodes = markdown_text_to_textNodes(line)
+
+        li_children = []
+        for text_node in text_nodes:
+            li_children.append(text_node_to_html_node(text_node))
+
+        list_element = ParentNode(li_children, "li", None)
+        children.append(list_element)
+
+    return ParentNode(children, "ul", None)
+
+
+def ordered_list_to_html(block: str) -> HTMLNode:
+    """Each item should be enclosed in li tag"""
+    children = []
+
+    lines = list(map(lambda x: x[2:], block.splitlines()))
+
+    for line in lines:
+        text_nodes = markdown_text_to_textNodes(line)
+
+        li_children = []
+        for text_node in text_nodes:
+            li_children.append(text_node_to_html_node(text_node))
+
+        list_element = ParentNode(li_children, "li", None)
+        children.append(list_element)
+
+    return ParentNode(children, "ol", None)
--- StaticSiteGenerator/tests/tests_markdown.py
+++ StaticSiteGenerator/tests/tests_markdown.py
@@ -1,18 +1,133 @@
 import unittest
 
-from src.HTMLNode import HTMLNode, LeafNode, ParentNode
-from src.markdown import markdown_to_html, paragraph_to_html
+from src.HTMLNode import LeafNode, ParentNode
+from src.markdown import (
+    codeblock_to_html,
+    heading_to_html,
+    ordered_list_to_html,
+    paragraph_to_html,
+    quoteblock_to_html,
+    unordered_list_to_html,
+)
 
 
 class TestMarkdownToHTML(unittest.TestCase):
     def test_simple(self):
         self.fail()
 
+
+class TestMarkdownToHTMLUtils(unittest.TestCase):
     def test_paragraph(self):
+        # Actual
         block = "hello\nworld"
         html = paragraph_to_html(block)
 
+        # Expected
         children = [LeafNode(None, "hello\nworld", None)]
         expected = ParentNode(children, tag="p")
+
+        self.assertEqual(html, expected)
+
+    def test_paragraph_formatted(self):
+        # Actual
+        block = "he*ll*o\n**world**\n"
+        html = paragraph_to_html(block)
+
+        # Expected
+        word = lambda x: LeafNode(None, x)
+        italic_word = LeafNode("i", "ll")
+        bold_word = LeafNode("b", "world")
+
+        children = [word("he"), italic_word, word("o\n"), bold_word, word("\n")]
+        expected = ParentNode(children, tag="p")
+
+        # Assert
+        self.assertEqual(html, expected)
+
+    def test_quouteblock(self):
+        # Actual
+        block = """>this is a quote
+>by a **really** smart
+>*guy*"""
+        html = quoteblock_to_html(block)
+
+        # Expected
+        children = [LeafNode(None, "this is a quote\nby a ")]
+        children.append(LeafNode("b", "really"))
+        children.append(LeafNode(None, " smart\n"))
+        children.append(LeafNode("i", "guy"))
+        expected = ParentNode(children, tag="blockquote")
+
+        self.assertEqual(html, expected)
+
+    def test_codeblock(self):
+        # Actual
+        code = """this is a code
+>by a **really** smart
+>*guy*"""
+        block = f"```{code}```"
+        html = codeblock_to_html(block)
+
+        # Expected
+        children = [LeafNode(None, code)]
+        expected = ParentNode(children, tag="code")
+
+        self.assertEqual(html, expected)
+
+    def test_heading_h1(self):
+        # Actual
+        headings = "# "
+        sample_text = """Heading test
+more text in new lines
+yes"""
+        block = f"{headings}{sample_text}"
+        html = heading_to_html(block)
+
+        # Expected
+        children = [LeafNode(None, sample_text)]
+        expected = ParentNode(children, tag="h1")
+
+        self.assertEqual(html, expected)
+
+    def test_heading_h6(self):
+        # Actual
+        headings = "###### "
+        sample_text = """Heading test
+more text in new lines
+yes"""
+        block = f"{headings}{sample_text}"
+        html = heading_to_html(block)
+
+        # Expected
+        children = [LeafNode(None, sample_text)]
+        expected = ParentNode(children, tag="h6")
+
+        self.assertEqual(html, expected)
+
+    def test_unordered_list(self):
+        # Actual
+        elements = ["first", "second", "third"]
+        unordered_elements = [f"* {elem}" for elem in elements]
+        block = "\n".join(unordered_elements)
+
+        html = unordered_list_to_html(block)
+
+        # Expected
+        children = [ParentNode([LeafNode(None, elem)], "li") for elem in elements]
+        expected = ParentNode(children, tag="ul")
+
+        self.assertEqual(html, expected)
+
+    def test_ordered_list(self):
+        # Actual
+        elements = ["first", "second", "third"]
+        unordered_elements = [f"{idx+1}.{elem}" for idx, elem in enumerate(elements)]
+        block = "\n".join(unordered_elements)
+
+        html = ordered_list_to_html(block)
+
+        # Expected
+        children = [ParentNode([LeafNode(None, elem)], "li") for elem in elements]
+        expected = ParentNode(children, tag="ol")
 
         self.assertEqual(html, expected)
